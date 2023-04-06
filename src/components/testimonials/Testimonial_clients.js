e8035766bf11df1690e2fcd9bff87e8afa8da63f
subham-04 <subham0422@gmail.com>
2023-04-06 11:38:11 +0530

    2nd review

---
+++ src/components/testimonials/Testimonial_clients.js
@@ -0,0 +1,26 @@
+import Krishnendu from '../../assets/testimonial_img/Krishnendu Nandi.jpg';
+import Kaustav from '../../assets/testimonial_img/kaustav.jpeg'
+
+const Data = [
+    
+    {
+        avatar: Krishnendu,
+          name: 'Krishnendu Nandi',
+          comapny_name:"Accenture",
+          review: 'I know Subham for quite some times as he is my junior from same college. We have come across many times during hackathons, different coding competitions. I have seen a few folks having the amount of enthusiasm and learning capacity Subham has. He is truly a smart-working geek. And last but not the least, he is a good human being.'
+    },
+    {
+        avatar:Kaustav,
+        name: 'Kaustav Basak',
+        comapny_name: "Steel Eye",
+        review: 'Hardworking lad'
+    }
+
+
+
+    
+
+  ];
+
+
+  export default Data;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './testimonials.css'
-import Krishnendu from '../../assets/testimonial_img/Krishnendu Nandi.jpg';
+import Data from './Testimonial_clients'
 
 
 // import required modules
@@ -20,19 +20,6 @@ import "swiper/css/pagination";
 
 
 
-const data = [
-    
-    {
-        avatar: Krishnendu,
-          name: 'Krishnendu Nandi',
-          comapny_name:"Accenture",
-          review: 'I know Subham for quite some times as he is my junior from same college. We have come across many times during hackathons, different coding competitions. I have seen a few folks having the amount of enthusiasm and learning capacity Subham has. He is truly a smart-working geek. And last but not the least, he is a good human being.'
-    }
-
-
-    
-
-  ];
 
 
 function Testimonials() {
@@ -52,7 +39,7 @@ function Testimonials() {
       modules={[Pagination]}
       className="mySwiper container testimonials__container">
       {
-        data.map(({avatar,name,review,comapny_name},index)=>{
+        Data.map(({avatar,name,review,comapny_name},index)=>{
           return(
             <SwiperSlide className="testimonial">
             <div key={index} className="client__avatar">
